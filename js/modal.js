f8689b0a2edf492c1de565ea9d5d75134e9279a5
Radina Yotova <44052854+GradinaYotova@users.noreply.github.com>
2022-03-29 01:17:16 +0200

    push-

--- js/modal.js
+++ js/modal.js
@@ -4,3 +4,16 @@ $( ".project-spine" ).each(function(index) {
     $(this).siblings().removeClass('active');
   });
 });
+
+
+$("#fleur").mouseenter(
+   function() {
+   	$('#fleur-text').html('Fleurine Brijker');  
+   }
+);
+
+$("#fleur").mouseleave(
+	function() {
+   	 $('#fleur-text').html('Van Zee Tot Land');
+   	}
+);
