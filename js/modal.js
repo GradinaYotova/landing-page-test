328d8289d291512e1d4e588e3d227ef221247a8c
Radina Yotova <44052854+GradinaYotova@users.noreply.github.com>
2022-03-30 23:38:10 +0200

    push-all-new

--- js/modal.js
+++ js/modal.js
@@ -6,24 +6,6 @@ $( ".project-spine" ).each(function(index) {
 });
 
 
-// $("#fleur").mouseenter(
-//    function() {
-//    	$('#fleur-text').html('Fleurine Brijker');  
-//    }
-// );
-
-// $("#fleur").mouseleave(
-// 	function() {
-//    	 $('#fleur-text').html('Van Zee Tot Land');
-//    	}
-// );
-
-// $("#fleur").hover(
-// 	function() {
-//    	 $('#fleur-text').html('Van Zee Tot Land');
-//    	}
-// );
-
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   
   $(function() {
@@ -42,7 +24,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
         , //true or false - should the marquee be duplicated to show an effect of continues flow
         duplicated: false
         , //speed in milliseconds of the marquee in milliseconds
-        duration: 4000
+        duration: 6000
         , //gap in pixels between the tickers
         gap: 0
         , //on cycle pause the marquee
@@ -72,7 +54,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
         , //true or false - should the marquee be duplicated to show an effect of continues flow
         duplicated: false
         , //speed in milliseconds of the marquee in milliseconds
-        duration: 4000
+        duration: 6000
         , //gap in pixels between the tickers
         gap: 0
         , //on cycle pause the marquee
@@ -104,7 +86,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
           , //true or false - should the marquee be duplicated to show an effect of continues flow
           duplicated: false
           , //speed in milliseconds of the marquee in milliseconds
-          duration: 4000
+          duration: 6000
           , //gap in pixels between the tickers
           gap: 0
           , //on cycle pause the marquee
@@ -132,7 +114,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
         , //true or false - should the marquee be duplicated to show an effect of continues flow
         duplicated: false
         , //speed in milliseconds of the marquee in milliseconds
-        duration: 4000
+        duration: 6000
         , //gap in pixels between the tickers
         gap: 0
         , //on cycle pause the marquee
@@ -162,7 +144,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
             , //true or false - should the marquee be duplicated to show an effect of continues flow
             duplicated: false
             , //speed in milliseconds of the marquee in milliseconds
-            duration: 4000
+            duration: 6000
             , //gap in pixels between the tickers
             gap: 0
             , //on cycle pause the marquee
@@ -174,33 +156,6 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
           });
     });
 
-    // $(function() {
-    //   $('.marquee-up')
-    //     .marquee({
-    //       //If you wish to always animate using jQuery
-    //       allowCss3Support: true
-    //       , //works when allowCss3Support is set to true - for full list see http://www.w3.org/TR/2013/WD-css3-transitions-20131119/#transition-timing-function
-    //       css3easing: 'linear'
-    //       , //requires jQuery easing plugin. Default is 'linear'
-    //       easing: 'linear'
-    //       , //pause time before the next animation turn in milliseconds
-    //       delayBeforeStart: 0
-    //       , //'left', 'right', 'up' or 'down'
-    //       direction: 'up'
-    //       , //true or false - should the marquee be duplicated to show an effect of continues flow
-    //       duplicated: false
-    //       , //speed in milliseconds of the marquee in milliseconds
-    //       duration: 4000
-    //       , //gap in pixels between the tickers
-    //       gap: 0
-    //       , //on cycle pause the marquee
-    //       pauseOnCycle: false
-    //       , //on hover pause the marquee - using jQuery plugin https://github.com/tobia/Pause
-    //       pauseOnHover: false
-    //       , //the marquee is visible initially positioned next to the border towards it will be moving
-    //       startVisible: false
-    //     });
-    // });
 
     $('.project-spine').each(function(){
    
